2f0105f4dd28fb307fa99088bcb30e2e0318dc73
Sherin Binu <Sherin Binu>
2017-04-29 10:34:42 +0530

    Project created

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tuthoth</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" href="css/normalize.css">
+    </head>
+        <body>
+        </body>
+</html>
