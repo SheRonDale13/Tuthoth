c99f64b73f190287f1bd65fbefd76d104ba2de2e
Sherin Binu <Sherin Binu>
2017-04-29 15:51:22 +0530

    finished front-end template

---
+++ templates/include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo htmlspecialchars($results['pageTitle']) ?></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<a href="."><img id="logo" src="logo.jpg" alt="Tuthoth"></a>
+		<div class="footer">
+			Tuthoth &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
+		</div>
+	</div>
+</body>
+</html>
