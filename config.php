065d791de531a4416a1d296c5ce50c11b7843d8b
Sherin Binu <Sherin Binu>
2017-04-29 16:20:26 +0530

    debugged few errors

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 	ini_set("display_errors", true);
 	date_default_timezone_set("Indian/Christmas");
 	define("DB_DSN", "mysql:host=localhost;dbname=cms");
-	define("DB_USERNAME", "username");
-	define("DB_PASSWORD", "password");
+	define("DB_USERNAME", "root");
+	define("DB_PASSWORD", "");
 	define("CLASS_PATH", "classes");
 	define("TEMPLATE_PATH", "templates");
 	define("HOMEPAGE_NUM_ARTICLES", 5);
@@ -11,11 +11,11 @@
 	define("ADMIN_PASSWORD", "mypass");
 	require(CLASS_PATH . "/Article.php");
 
-	function handleException($exception) {
+/*	function handleException($exception) {
 		echo "Sorry, a problem occurred. Please try later.";
 		error_log($exception -> getMessage());
 	}
-	set_exception_handler('handleException');
+	set_exception_handler('handleException');*/
 ?>
 
 
