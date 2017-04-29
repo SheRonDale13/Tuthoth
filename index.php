e42c3dbe92462e0b85d1bf487fd08f1c49cfe522
Sherin Binu <Sherin Binu>
2017-04-29 14:43:49 +0530

    updated index.php

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Tuthoth</title>
-        <link rel="stylesheet" type="text/css" href="/Tuthoth/css/main.css">
-        <link rel="stylesheet" href="/Tuthoth/css/normalize.css">
-    </head>
-        <body>
-        <div class="container">
-        	<div class="header">
-        		<h1>Tuthoth</h1>
-        	</div>
-        	<div class="content-container">
-        		Welcome to our website
-        	</div>
-        </div>
-        </body>
-</html>
+<?php
+	require("config.php");
+	$action = isset($_GET['action']) ? $_GET['action'] : "";
+	switch($action) {
+		case 'archive':
+			archive();
+			break;
+		case 'viewArticle':
+			viewArticle();
+			brea;
+		case default:
+			homepage();
+	}
+
+	function archive() {
+		$results = array();
+		$data = Article::getList();
+		$results['articles'] = $data['results'];
+		$results['totalRows'] = $data['totalRows'];
+		$results['pageTitle'] = "Article Archive | Tuthoth";
+		require(TEMPLATE_PATH."/archive.php");
+	}
+
+	function viewArticle() {
+		if(!isset($_GET['articleId']) || !$_GET['articleId']) {
+			homepage();
+			return;
+		}
+		$results = array();
+		$results['article'] = Articles::getById((int)$_GET['articleId']);
+		$results['pageTitle'] = $results['article']->title." | Tuthoth";
+		require(TEMPLATE_PATH."/viewArticle.php");
+	}
+
+	function homepage() {
+		$results = array();
+		$data = Article::getList(HOMEPAGE_NUM_ARTICLES);
+		$results['articles'] = $data['results'];
+		$results['totalRows'] = $data['totalRows'];
+		$results['pageTitle'] = "Tuthoth";
+		require(TEMPLATE_PATH."/homepage.php");
+	}
+?>
