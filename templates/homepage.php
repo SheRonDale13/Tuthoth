f2cc1db791ff19dd0a68f13ae12d68ef64b81eed
Sherin Binu <Sherin Binu>
2017-04-30 08:24:04 +0530

    changed headlines from ul to table

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,22 +1,26 @@
 <?php
 	include("templates/include/header.php");
 ?>
-<ul class="headlines">
+<table>
 	<?php
 		foreach($results['articles'] as $article) {?>
-		<li>
-			<h2>
+		<tr>
+			<td>
 				<span class="pubDate"><?php echo date('j F', $article->publicationDate)?></span>
-				<a href=".?action=viewArticle&amp;articleId=<?php echo $article->id ?>">
-					<?php echo htmlspecialchars($article->title) ?>
-				</a>
-			</h2>
-			<p class="summary">
-				<?php echo htmlspecialchars($article->summary) ?>
-			</p>
-		</li>
+			</td>
+			<td>
+				<h2>
+					<a href=".?action=viewArticle&amp;articleId=<?php echo $article->id ?>">
+						<?php echo htmlspecialchars($article->title) ?>
+					</a>
+				</h2>
+				<p class="summary">
+					<?php echo htmlspecialchars($article->summary) ?>
+				</p>
+			</td>
+		</tr>
 	<?php } ?>
-</ul>
+</table>
 <p class="homepage-archive"><a href="./?action=archive">Article Archive</a></p>
 <?php
 	include("templates/include/footer.php");
--- templates/include/header.php
+++ templates/include/header.php
@@ -4,8 +4,11 @@
 	<meta charset="UTF-8">
 	<title><?php echo htmlspecialchars($results['pageTitle']) ?></title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Bungee+Inline" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
-		<a href="."><img class="logo" src="./images/logo.gif" alt="Tuthoth"></a>
-
+		<div class="header">
+			<h1 class="title">TUTHOTH</h1>
+			<a class="logo-container" href="."><img class="logo" src="./images/logo.gif" alt="Tuthoth"></a>
+		</div>
